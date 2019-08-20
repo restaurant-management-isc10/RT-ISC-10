64dca947b06e344747920427960fc96ec03c8616
Kiệt Mojito Chanh <50434591+kiettd1279@users.noreply.github.com>
2019-08-20 16:22:45 +0700

    Creat model
    by Kiệt TD

---
+++ MENU_TYPES.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, type) => {
+    return sequelize.define('MENU_TYPE', {
+        id: {
+            field: 'MT_ID',
+            type: type.STRING(8),
+            primaryKey: true
+        },
+        MT_Name: {
+            type: type.STRING(8),
+            allowNull: false
+        }
+    },{timestamps : false})
+}
